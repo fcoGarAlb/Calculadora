0603a7b671445d2f8e07bd3c940df81634d46b81
Francisco García Albaladejo <124467928+fcoGarAlb@users.noreply.github.com>
2023-02-27 16:07:18 +0100

    Update Calculadora.java

--- Calculadora2/src/calculadora2/Calculadora.java
+++ Calculadora2/src/calculadora2/Calculadora.java
@@ -1,3 +1,5 @@
+// Esto es un comentario nuevo para probar Github
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
